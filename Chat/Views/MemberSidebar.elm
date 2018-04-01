58bfeb80e2e6baca63eee5361de21a27bf938e81
Dan Luria <dan@dotdan.com>
2018-04-01 19:52:31 -0400

    Make member available in the sidebar.

--- Chat/Views/MemberSidebar.elm
+++ Chat/Views/MemberSidebar.elm
@@ -2,22 +2,30 @@ module Views.MemberSidebar exposing (memberSidebar)
 
 import Html exposing (Attribute, Html, div, text)
 import Html.Attributes exposing (..)
+import Models.Member
 import Types exposing (Model, Msg(..))
 
 
 memberSidebar : Model -> Html Msg
 memberSidebar model =
     let
-        memberId =
+        member =
             case model.selectedMemberId of
                 Just id ->
-                    toString id
+                    case Models.Member.getMemberById model.members id of
+                        Just member ->
+                            member
 
+                        -- This should never happen?
+                        Nothing ->
+                            Models.Member.missing
+
+                -- This should never happen?
                 Nothing ->
-                    "0"
+                    Models.Member.missing
     in
     div
         [ id "memberSidebar"
         , classList [ ( "showMemberSidebar", model.viewVisibility.memberSidebar ) ]
         ]
-        [ text ("Sidebar for " ++ memberId) ]
+        [ text ("Sidebar for " ++ member.name) ]
